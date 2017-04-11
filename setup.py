895414bd8ee04e97e86e7f260c224b359a703d07
Phuong <boingonline@gmail.com>
2017-04-11 11:20:34 +0700

    Update setup version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from setuptools import setup
 
 setup(name='emojipy',
-      version='0.1.2',
+      version='0.1.3',
       description='Python wrapper for emojione',
       author='Kaviraj Kanagaraj',
       author_email='kaviraj@launchyard.com',
@@ -11,5 +11,5 @@ setup(name='emojipy',
       test_suite='emojipy.tests',
       url = 'https://github.com/phuong/emojipy', 
       download_url = 'https://github.com/phuong/emojipy/archive/master.zip', 
-      keywords = ['emojipy', 'emojione'], 
+      keywords = ['emojipy', 'emojione', 'python emoji'], 
 )
