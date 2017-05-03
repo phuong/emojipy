6682844f8e6955dc6b9cf991bbf560aa9391ed66
Phuong <boingonline@gmail.com>
2017-05-03 16:24:15 +0700

    Avoid none-unicode chars

--- emojipy/emojipy.py
+++ emojipy/emojipy.py
@@ -161,11 +161,12 @@ class Emoji(object):
     def unicode_to_shortcode(cls, text):
         def replace_unicode(match):
             unicode_char = text[match.start():match.end()]
+            if ord(unicode_char) <= 127:
+                return unicode_char
             unicode_encoded = unicode_char.encode('utf-8')
             if not unicode_encoded or unicode_encoded not in unicode_replace:
                 return unicode_char  # unsupported unicode char
             return unicode_replace[unicode_encoded]
-
         text = re.sub(cls.unicode_compiled, replace_unicode, text)
         return text
 
