df9ba587e624ffdc81647fe3d3c09a06b04caef5
Phuong <boingonline@gmail.com>
2017-05-03 16:24:24 +0700

    Avoid none-unicode chars

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from setuptools import setup
 
 setup(name='emojipy',
-      version='3.0.1',
+      version='3.0.2',
       description='Python wrapper for emojione',
       author='Kaviraj Kanagaraj',
       author_email='kaviraj@launchyard.com',
